986e1b4957aa2bdbd456f29ff8bffa2b66528e60
Finn Woelm <public@finnwoelm.com>
2019-05-24 18:05:00 +0200

    Updates

---
+++ precache-manifest.321326ec1b03fda2b59206f3b728548f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6ae0f9263e452479a1b7b244660893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162743a62761380c70fc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ed42659c6ce125cd611d",
+    "url": "/static/js/2.7908df70.chunk.js"
+  },
+  {
+    "revision": "162743a62761380c70fc",
+    "url": "/static/js/main.3f13b94f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
