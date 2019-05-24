828d1dbc077a8352a800eb3f2b27ed1d32ba1203
Finn Woelm <public@finnwoelm.com>
2019-05-24 21:25:57 +0200

    Set up Material UI

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HashRouter, Route, Link } from "react-router-dom";
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 
 class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
+        <Container>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
 
-          <hr />
+            <hr />
 
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </div>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </Container>
       </HashRouter>
     );
   }
 }
 
-const Home = () => <div><h2>Home</h2></div>
+const Home = () => <div><Typography variant="h2" gutterBottom>Home</Typography></div>
 const About = () => <div><h2>About</h2></div>
 
 export default App;
