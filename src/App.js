c94b4332d6fa253c332166c8be3bcd40922fea44
Finn Woelm <public@finnwoelm.com>
2019-05-25 15:13:10 +0200

    Add header bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { HashRouter, Route, Link } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar'
 import Container from '@material-ui/core/Container'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import styled, { createGlobalStyle } from 'styled-components'
 import HomePage from './pages/HomePage'
 import CountryPage from './pages/CountryPage'
+
+const SiteHeader = styled(AppBar)`
+  && {
+    box-shadow: 0 1px 12px -3px rgba(0,0,0,.1);
+    background: white;
+    padding: 8px 0;
+  }
+
+  h6 {
+    font-weight: 400;
+  }
+`
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #f8f9fa;
+    margin: 0;
+    padding: 0;
+  }
+
+  .bg {
+    &.sdg1 {
+      background: rgb(204,44,67);
+    }
+    &.sdg2 {
+      background: rgb(211, 162 ,70 );
+    }
+    &.sdg3 {
+      background: rgb(81, 148 ,71 );
+    }
+    &.sdg4 {
+      background: rgb(175, 39, 56 );
+    }
+    &.sdg5 {
+      background: rgb(211 ,69 ,58 );
+    }
+    &.sdg6 {
+      background: rgb(73, 181 ,203 ) ;
+    }
+    &.sdg7 {
+      background: rgb(238, 189, 64 ) ;
+    }
+    &.sdg8 {
+      background: rgb(141, 36, 65 );
+    }
+    &.sdg9 {
+      background: rgb(219, 104, 62 ) ;
+    }
+    &.sdg10 {
+      background: rgb(197 ,36 ,96 );
+    }
+    &.sdg11 {
+      background: rgb(232, 151, 68 ) ;
+    }
+    &.sdg12 {
+      background: rgb(177 ,134, 58 ) ;
+    }
+    &.sdg13 {
+      background: rgb(65, 119 ,68 );
+    }
+    &.sdg15 {
+      background: rgb(100 ,167, 74 ) ;
+    }
+    &.sdg16 {
+      background: rgb(31, 101 ,137 ) ;
+    }
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
+        <SiteHeader position="static" color="default">
+        <Container style={{padding: 0}}>
+          <Toolbar>
+              <div style={{flexGrow: 1}}>
+                <Typography variant="h6" color="inherit">
+                  European Cities SDG Index 2019
+                </Typography>
+                <Typography variant="body1" style={{color: '#6c757d'}} gutterBottom>
+                  Prototype Version
+                </Typography>
+              </div>
+              <img src='/images/SDSN-logo.png' alt='SDSN logo' style={{maxHeight: 50}}/>
+            </Toolbar>
+          </Container>
+        </SiteHeader>
         <Container>
           <div>
             <ul>
@@ -23,6 +108,7 @@ class App extends Component {
             <Route path="/:country" component={CountryPage} />
           </div>
         </Container>
+        <GlobalStyle />
       </HashRouter>
     );
   }
--- src/components/SDGBar.js
+++ src/components/SDGBar.js
@@ -12,52 +12,6 @@ const Block = styled.div`
   border-right: 1px solid #fff;
   min-height: 24px;
   vertical-align: bottom;
-
-  &.SDG1 {
-    background: rgb(204,44,67);
-  }
-  &.SDG2 {
-    background: rgb(211, 162 ,70 );
-  }
-  &.SDG3 {
-    background: rgb(81, 148 ,71 );
-  }
-  &.SDG4 {
-    background: rgb(175, 39, 56 );
-  }
-  &.SDG5 {
-    background: rgb(211 ,69 ,58 );
-  }
-  &.SDG6 {
-    background: rgb(73, 181 ,203 ) ;
-  }
-  &.SDG7 {
-    background: rgb(238, 189, 64 ) ;
-  }
-  &.SDG8 {
-    background: rgb(141, 36, 65 );
-  }
-  &.SDG9 {
-    background: rgb(219, 104, 62 ) ;
-  }
-  &.SDG10 {
-    background: rgb(197 ,36 ,96 );
-  }
-  &.SDG11 {
-    background: rgb(232, 151, 68 ) ;
-  }
-  &.SDG12 {
-    background: rgb(177 ,134, 58 ) ;
-  }
-  &.SDG13 {
-    background: rgb(65, 119 ,68 );
-  }
-  &.SDG15 {
-    background: rgb(100 ,167, 74 ) ;
-  }
-  &.SDG16 {
-    background: rgb(31, 101 ,137 ) ;
-  }
 `
 
 class SDGBar extends PureComponent {
@@ -73,7 +27,7 @@ class SDGBar extends PureComponent {
       return (
         <Block
           key={sdg}
-          className={`sdg ${sdg}`}
+          className={`sdg bg ${sdg.toLowerCase()}`}
           data-sdg={sdg}
           data-score={`${scores[sdg]}%`}
           style={{width: `calc(${blockWidth}% - 1px`}}
