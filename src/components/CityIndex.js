d3d30f4920ac4f64c6c6732b62a1030487990b6b
Finn Woelm <public@finnwoelm.com>
2019-05-25 12:17:05 +0200

    Style the SDG Index table to match global SDG Index

--- src/components/CityIndex.js
+++ src/components/CityIndex.js
@@ -1,8 +1,93 @@
 import React, { PureComponent } from 'react'
+import ButtonBase from '@material-ui/core/ButtonBase'
 import Typography from '@material-ui/core/Typography'
 import MUIDataTable from "mui-datatables"
+import styled from 'styled-components'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
+import SvgIcon from '@material-ui/core/SvgIcon'
 
 import SDGBar from './SDGBar'
+
+const SDGIndexTable = styled(MUIDataTable)`
+  && {
+    overflow-x: auto;
+  }
+
+  .MuiTableCell-head {
+    color: black;
+    font-size: 1rem;
+    border-top: 1px solid rgba(0,0,0,0.12);
+
+    &:after{
+      content:'';
+      position:absolute;
+      left: 0;
+      bottom: 0;
+      width:100%;
+      border-bottom: 2px solid rgba(0,0,0,0.12);
+    }
+  }
+
+  .MuiTableCell-body {
+    color: #6c757d;
+    font-size: 1rem;
+  }
+`
+
+const SDGIndexTableHead = styled.th`
+  && {
+    border-top: 1px solid rgba(0,0,0,0.12);
+    top: 0px;
+    left: 0px;
+    z-index: 100;
+    position: sticky;
+    background-color: #fff;
+    padding: 0;
+
+    &:after{
+      content:'';
+      position:absolute;
+      left: 0;
+      bottom: 0;
+      width:100%;
+      border-bottom: 2px solid rgba(0,0,0,0.12);
+    }
+  }
+
+  button {
+    width: 100%;
+    padding: 20px 16px 20px 16px;
+    justify-content: left;
+    font-weight: bold;
+    line-height: 24px;
+
+    &:hover {
+      background: #eee;
+    }
+  }
+`
+
+const customHead = ({index, sort, sortDirection, label}, sortColumn) => {
+  const sortIndicator = (() => {
+    switch(sortDirection) {
+      case 'asc': return <ArrowUpwardIcon />
+      case 'desc': return <ArrowDownwardIcon />
+      default: return <SvgIcon />
+    }
+  })();
+
+  return (
+    <SDGIndexTableHead key={index}>
+      <ButtonBase disabled={!sort} onClick={() => sortColumn(index)}>
+        <Typography variant='body1' style={{fontWeight: 500}}>
+          {label}
+        </Typography>
+        {sortIndicator}
+      </ButtonBase>
+    </SDGIndexTableHead>
+  );
+}
 
 const options = {
   download: false,
@@ -14,6 +99,7 @@ const options = {
   elevation: 0,
   rowsPerPage: 500,
   search: false,
+  sort: true,
   customFooter: (count, page, rowsPerPage, changeRowsPerPage, changePage) => {
     return <tbody></tbody>
   }
@@ -64,13 +150,20 @@ class CityIndex extends PureComponent {
       {
         name: "City",
         options: {
-          sort: true
+          sort: true,
+          sortDirection: null,
+          customHeadRender: customHead,
+          customBodyRender: (value, tableMeta, updateValue) => {
+            return <span style={{fontWeight: '500', color: 'black'}}>{value}</span>
+          },
         },
       },
       {
         name: "Region",
         options: {
-          sort: true
+          sort: true,
+          sortDirection: null,
+          customHeadRender: customHead
         },
       },
       {
@@ -81,33 +174,37 @@ class CityIndex extends PureComponent {
             return <SDGBar scores={value}
                            showTooltip={showTooltip}
                            hideTooltip={hideTooltip}/>;
-          }
+          },
+          customHeadRender: customHead
         },
       },
       {
         name: "Rank",
         options: {
-          sort: true
+          sort: true,
+          sortDirection: 'asc',
+          customHeadRender: customHead
         },
       },
       {
         name: "Score",
         options: {
-          sort: false
+          sort: false,
+          customHeadRender: customHead
         },
       },
     ]
 
     return (
       <div>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h5" gutterBottom>
           The European Cities SDG Index
         </Typography>
-        <Typography variant='body1'>
+        <Typography variant='body1' style={{color: '#6c757d'}} gutterBottom>
           Select a city to see its full profile.
         </Typography>
 
-        <MUIDataTable
+        <SDGIndexTable
           data={data}
           columns={columns}
           options={options}
